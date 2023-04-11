8b7d0d9c91ad1d9329ac3511a1db32f100d8f1cd
Sivakumar Adchayan <sivaadchu@gmail.com>
2023-04-11 19:15:51 +0530

    splash code

--- app/src/main/java/com/jhc/ygc/MainActivity.java
+++ app/src/main/java/com/jhc/ygc/MainActivity.java
@@ -2,13 +2,16 @@ package com.jhc.ygc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);;
+        setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/jhc/ygc/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.jhc.ygc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashScreen extends AppCompatActivity {
+
+    Button LetGo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        LetGo = findViewById(R.id.button);
+
+        LetGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
