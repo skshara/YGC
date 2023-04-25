2032f6ac00c7ffc36800f490514f0a83a15fe53c
Sharavanapriyan <ksharavanapriyan@gmail.com>
2023-04-25 19:44:03 +0530

    update

---
+++ app/src/main/java/com/jhc/ygc/MainActivity.java
@@ -0,0 +1,69 @@
+package com.jhc.ygc;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.jhc.ygc.databinding.ActivityMain2Binding;
+
+public class MainActivity extends AppCompatActivity {
+    Button signOut;
+    FirebaseAuth fAuth;
+    //FirebaseUser fUser;
+    //DataSnapshot dataSnapshot;
+
+    //protected void onStart() {
+    // super.onStart();
+    // String grade = getGrade();
+    // if(grade==null) {
+    // Go to register for now
+    //fAuth.getInstance().signOut();
+    //Toast.makeText(MainActivity.this, "Google sign up is not allowed. First Sign up with email", Toast.LENGTH_SHORT).show();
+    //startActivity(new Intent(getApplicationContext(),login.class));
+    //finish();
+    //}
+    //}
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main2);
+        signOut = findViewById(R.id.signOut);
+        //fAuth.getInstance();
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),login.class));
+                finish();
+            }
+        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
