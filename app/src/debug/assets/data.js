19d9f32c98f58d1433ffa1265deb2ada6fa20851
Sivakumar Adchayan <sivaadchu@gmail.com>
2023-06-14 20:43:37 +0530

    changes++

--- app/src/debug/assets/data.js
+++ app/src/debug/assets/data.js
@@ -12,39 +12,51 @@ let songs = [
         cover: 'assets/images/cover 2.png'
     },
     {
-        name: 'song 3',
-        path: 'assets/musics/Song 3.mp3',
-        artist: 'artist 3',
+        name: 'Alagana Palani Malai Aandava',
+        path: 'assets/musics/y2mate.com - அழகன பழன மல ஆணடவ Alagana palani Malai andava.mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 3.png'
     },
     {
-        name: 'song 4',
-        path: 'assets/musics/Song 4.mp3',
-        artist: 'artist 4',
+        name: 'Ullam Irangi Vanthu - Keerthanai',
+        path: 'assets/musics/y2mate.com - கரததனஉளளம இரஙக வநத Keerthanai Ullam iranki vanthu Hamsathvani ragam.mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 4.png'
     },
     {
-        name: 'song 5',
-        path: 'assets/musics/Song 5.mp3',
-        artist: 'artist 5',
+        name: 'Kuuvi alaiththal',
+        path: 'assets/musics/y2mate.com - கவ அழததல கரல கடபபன வலஜ இரகமKuuvi alaiththalValaji ragam.mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 5.png'
     },
     {
-        name: 'song 6',
-        path: 'assets/musics/Song 6.mp3',
-        artist: 'artist 6',
+        name: 'Santhanathaye Araithu',
+        path: 'assets/musics/y2mate.com - கவடசசநத சநதனததய அரதததரம 9 கரநடக சஙகதம Kaavadi sinthu Grade 9 music practical.mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 6.png'
     },
     {
-        name: 'song 7',
-        path: 'assets/musics/Song 7.mp3',
-        artist: 'artist 7',
+        name: 'Sannithi Velavan',
+        path: 'assets/musics/y2mate.com - சநநத வலவன பகதப படலSannithi velavan bakthi isai (1).mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 7.png'
     },
     {
-        name: 'song 8',
-        path: 'assets/musics/Song 8.mp3',
-        artist: 'artist 8',
+        name: 'Nervili thevaram',
+        path: 'assets/musics/y2mate.com - மனன நரவழ தவரமMaanin nervili Thevaram.mp3',
+        artist: 'e-Carnatic tamil',
         cover: 'assets/images/cover 8.png'
+    },
+    {
+        name: 'Madayaye Nithi',
+        path: 'assets/musics/y2mate.com - மதய நத சகக கலக கரத வசநத இரகம MAA dayai nithi Vasantha ragam.mp3',
+        artist: 'e-Carnatic tamil',
+        cover: 'assets/images/cover 4.png'
+    },
+    {
+        name: 'Vilayada Ithu Nerama',
+        path: 'assets/musics/y2mate.com - வளயட இத நரமஷணமகபரய இரகம Vilajada itu nerama_Shanmugapriya raga song.mp3',
+        artist: 'e-Carnatic tamil',
+        cover: 'assets/images/cover 1.png'
     },
 ]
