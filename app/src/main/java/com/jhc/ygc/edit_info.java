b2dc3728c1459d563d0b731889626cf5ac314051
Sivakumar Adchayan <sivaadchu@gmail.com>
2023-06-16 21:05:45 +0530

    Coded for profile edit

--- app/src/main/java/com/jhc/ygc/edit_info.java
+++ app/src/main/java/com/jhc/ygc/edit_info.java
@@ -1,17 +1,143 @@
 package com.jhc.ygc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class edit_info extends AppCompatActivity {
 
     TextInputEditText mEmail,mPassword,mGrade;
+    Button update;
+    FirebaseAuth fAuth;
+    FirebaseUser fUser;
+    FirebaseFirestore fStore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_info);
+        update = findViewById(R.id.updatebtn);
+        mEmail = findViewById(R.id.chemail);
+        mPassword = findViewById(R.id.chpwd);
+        mGrade = findViewById(R.id.chgrade);
+        fAuth = fAuth.getInstance();
+        fUser = fAuth.getCurrentUser();
+        fStore = FirebaseFirestore.getInstance();
+
+
+       update.setOnClickListener(view -> {
+           if(fUser!=null) {
+               String email = mEmail.getText().toString().trim();
+               String password = mPassword.getText().toString().trim();
+               String grade = mGrade.getText().toString().trim();
+               if (!TextUtils.isEmpty(email)) {
+                   // Update email
+                   fUser.updateEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                       @Override
+                       public void onSuccess(Void unused) {
+                           Toast.makeText(edit_info.this, "Successfully changed email", Toast.LENGTH_SHORT).show();
+                       }
+                   }).addOnFailureListener(new OnFailureListener() {
+                       @Override
+                       public void onFailure(@NonNull Exception e) {
+                           Toast.makeText(edit_info.this, "Failed to change email", Toast.LENGTH_SHORT).show();
+                       }
+                   });
+               }
+               if (!TextUtils.isEmpty(password)) {
+                   // Update password
+                   if (password.length() < 8) {
+                       mPassword.setError("Password should be at least 8 characters long");
+                   } else {
+                       fUser.updatePassword(password).addOnSuccessListener(new OnSuccessListener<Void>() {
+                           @Override
+                           public void onSuccess(Void unused) {
+                               Toast.makeText(edit_info.this, "Successfully updated password", Toast.LENGTH_SHORT).show();
+                           }
+                       }).addOnFailureListener(new OnFailureListener() {
+                           @Override
+                           public void onFailure(@NonNull Exception e) {
+                               Toast.makeText(edit_info.this, "Failed to update password: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                           }
+                       });
+                   }
+               }
+               if (!TextUtils.isEmpty(grade)) {
+                   // Update grade
+                   String fUserUid = fUser.getUid();
+                   DocumentReference db = fStore.collection("user").document(fUserUid);
+                   db.get().addOnSuccessListener(documentSnapshot -> {
+                       if (documentSnapshot.exists()) {
+                           Map<String, Object> userData = documentSnapshot.getData();
+                           if (userData != null) {
+                               Map<String, Object> user = new HashMap<>();
+                               user.put("grade", grade);
+                               db.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                   @Override
+                                   public void onSuccess(Void unused) {
+                                       Toast.makeText(edit_info.this, "Successfully changed grade", Toast.LENGTH_SHORT).show();
+                                   }
+                               }).addOnFailureListener(new OnFailureListener() {
+                                   @Override
+                                   public void onFailure(@NonNull Exception e) {
+                                       Toast.makeText(edit_info.this, "Failed to change grade", Toast.LENGTH_SHORT).show();
+                                   }
+                               });
+                           }
+                       } else {
+                           String userID = fAuth.getCurrentUser().getUid();
+                           DocumentReference documentReference = fStore.collection("user").document(userID);
+                           Map<String, Object> user = new HashMap<>();
+                           user.put("grade", grade);
+                           documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                               @Override
+                               public void onSuccess(Void unused) {
+                                   Toast.makeText(edit_info.this, "Successfully changed grade", Toast.LENGTH_SHORT).show();
+                               }
+                           }).addOnFailureListener(new OnFailureListener() {
+                               @Override
+                               public void onFailure(@NonNull Exception e) {
+                                   Toast.makeText(edit_info.this, "Failed to change grade", Toast.LENGTH_SHORT).show();
+                               }
+                           });
+
+                       }
+                   }).addOnFailureListener(e -> {
+                       // Failed with error code e
+                       String userID = fAuth.getCurrentUser().getUid();
+                       DocumentReference documentReference = fStore.collection("user").document(userID);
+                       Map<String, Object> user = new HashMap<>();
+                       user.put("grade", grade);
+                       documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                           @Override
+                           public void onSuccess(Void unused) {
+                               Toast.makeText(edit_info.this, "Successfully changed grade", Toast.LENGTH_SHORT).show();
+                           }
+                       }).addOnFailureListener(new OnFailureListener() {
+                           @Override
+                           public void onFailure(@NonNull Exception e) {
+                               Toast.makeText(edit_info.this, "Failed to change grade", Toast.LENGTH_SHORT).show();
+                           }
+                       });
+                   });
+               }
+           }
+       });
+
     }
 }
